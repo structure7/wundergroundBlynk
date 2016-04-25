e7f169003aa8e33c3a1d6d62dd63dcf8a3f5e3c1
structure7 <kemperm@usa.net>
2016-04-25 15:53:11 -0700

    Added files via upload

---
+++ wundergroundBlynk.ino
@@ -0,0 +1,163 @@
+// v4: Working with Blynk connection.
+// v5: Send to app vpin
+// v6: Send the real WU to Blynk
+// v7: Cut it down!
+
+#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+#include <SimpleTimer.h>
+#include <ArduinoJson.h>
+
+char auth[] = "fromBlynkApp *04";
+
+SimpleTimer timer;
+
+// The original time entries that didn't like being removed.
+#define DELAY_NORMAL    (10) 
+#define DELAY_ERROR     (00)
+
+#define WUNDERGROUND "api.wunderground.com"
+
+// HTTP request
+const char WUNDERGROUND_REQ[] =
+  "GET /api/[WU_API_KEY]/conditions/q/pws:KAZTEMPE29.json HTTP/1.1\r\n"   
+  "User-Agent: ESP8266/0.1\r\n"
+  "Accept: */*\r\n"
+  "Host: " WUNDERGROUND "\r\n"
+  "Connection: close\r\n"
+  "\r\n";
+
+float temp_f;
+
+void setup()
+{
+  Serial.begin(115200);
+  Blynk.begin(auth, "ssid", "pw");
+
+  while (Blynk.connect() == false) {
+    // Wait until connected
+  }
+  timer.setInterval(300000L, sendWU); // 5 minutes between Wunderground API calls.
+  timer.setInterval(90000L, sendWUtoBlynk); // 1.5 minutes between API data updates to Blynk. 
+
+  static char respBuf[4096];
+}
+
+static char respBuf[4096];
+
+bool showWeather(char *json);
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
+
+void sendWUtoBlynk()
+{
+    Blynk.virtualWrite(12, temp_f); // Write to Blynk app virtual pin 12
+}
+
+void sendWU()
+{
+  // Open socket to WU server port 80
+  Serial.print(F("Connecting to "));
+  Serial.println(WUNDERGROUND);
+
+  // Use WiFiClient class to create TCP connections (A LITTLE WORRIED HOW THIS WILL WORK WITH BLYNK)
+  WiFiClient httpclient;
+  const int httpPort = 80;
+  if (!httpclient.connect(WUNDERGROUND, httpPort)) {
+    Serial.println(F("connection failed"));
+    delay(DELAY_ERROR);
+    return;
+  }
+
+  // This will send the http request to the server
+  Serial.print(WUNDERGROUND_REQ);
+  httpclient.print(WUNDERGROUND_REQ);
+  httpclient.flush();
+
+  // Collect http response headers and content from Weather Underground
+  // HTTP headers are discarded.
+  // The content is formatted in JSON and is left in respBuf.
+  int respLen = 0;
+  bool skip_headers = true;
+  while (httpclient.connected() || httpclient.available()) {
+    if (skip_headers) {
+      String aLine = httpclient.readStringUntil('\n');
+      //Serial.println(aLine);
+      // Blank line denotes end of headers
+      if (aLine.length() <= 1) {
+        skip_headers = false;
+      }
+    }
+    else {
+      int bytesIn;
+      bytesIn = httpclient.read((uint8_t *)&respBuf[respLen], sizeof(respBuf) - respLen);
+      Serial.print(F("bytesIn ")); Serial.println(bytesIn);
+      if (bytesIn > 0) {
+        respLen += bytesIn;
+        if (respLen > sizeof(respBuf)) respLen = sizeof(respBuf);
+      }
+      else if (bytesIn < 0) {
+        Serial.print(F("read error "));
+        Serial.println(bytesIn);
+      }
+    }
+    delay(1);
+  }
+  httpclient.stop();
+
+  if (respLen >= sizeof(respBuf)) {
+    Serial.print(F("respBuf overflow "));
+    Serial.println(respLen);
+    delay(DELAY_ERROR);
+    return;
+  }
+  // Terminate the C string
+  respBuf[respLen++] = '\0';
+  Serial.print(F("respLen "));
+  Serial.println(respLen);
+  //Serial.println(respBuf);
+
+
+// This part will be removed in favor of the timer feature. Probably remove delay_error and just report ERR.
+  if (showWeather(respBuf)) {
+    delay(DELAY_NORMAL);
+  }
+  else {
+    delay(DELAY_ERROR);
+  }
+
+}
+
+bool showWeather(char *json)
+{
+  StaticJsonBuffer<3*1024> jsonBuffer;
+
+  // Skip characters until first '{' found
+  // Ignore chunked length, if present
+  char *jsonstart = strchr(json, '{');
+  //Serial.print(F("jsonstart ")); Serial.println(jsonstart);
+  if (jsonstart == NULL) {
+    Serial.println(F("JSON data missing"));
+    return false;
+  }
+  json = jsonstart;
+
+  // Parse JSON
+  JsonObject& root = jsonBuffer.parseObject(json);
+  if (!root.success()) {
+    Serial.println(F("jsonBuffer.parseObject() failed"));
+    return false;
+  }
+
+  // Extract weather info from parsed JSON
+  JsonObject& current = root["current_observation"];
+  temp_f = current["temp_f"];  // Was `const float temp_f = current["temp_f"];`
+  Serial.print(temp_f, 1); Serial.print(F(" F "));
+
+  return true;
+}
